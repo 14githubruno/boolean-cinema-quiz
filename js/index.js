56032cb81a626c45b2e4e1e9472da29c10ec10d5
bruno <bruno.lusardi.96@gmail.com>
2023-12-21 16:24:52 +0100

    Fix bugs, Refact assertions, Add functionality for the user
    Now the quiz could be played and users can surely find out the number of their correct answers

--- js/index.js
+++ js/index.js
@@ -5,45 +5,135 @@ const seconds = {
   advanced: 20,
 };
 
-// assertions obj: 3 arrays for the 3 levels
-const assertions = {
-  basic: [
-    "Lorem Basic 1",
-    "Lorem Basic 2",
-    "Lorem Basic 3",
-    "Lorem Basic 4",
-    "Lorem Basic 5",
-    "Lorem Basic 6",
-    "Lorem Basic 7",
-    "Lorem Basic 8",
-    "Lorem Basic 9",
-    "Lorem Basic 10",
-  ],
-  medium: [
-    "Lorem Medium 1",
-    "Lorem Medium 2",
-    "Lorem Medium 3",
-    "Lorem Medium 4",
-    "Lorem Medium 5",
-    "Lorem Medium 6",
-    "Lorem Medium 7",
-    "Lorem Medium 8",
-    "Lorem Medium 9",
-    "Lorem Medium 10",
-  ],
-  advanced: [
-    "Lorem Advanced 1",
-    "Lorem Advanced 2",
-    "Lorem Advanced 3",
-    "Lorem Advanced 4",
-    "Lorem Advanced 5",
-    "Lorem Advanced 6",
-    "Lorem Advanced 7",
-    "Lorem Advanced 8",
-    "Lorem Advanced 9",
-    "Lorem Advanced 10",
-  ],
-};
+// three arrays of ten objects each
+const basic = [
+  {
+    assertion: "Lorem Basic 1",
+    value: true,
+  },
+  {
+    assertion: "Lorem Basic 2",
+    value: true,
+  },
+  {
+    assertion: "Lorem Basic 3",
+    value: false,
+  },
+  {
+    assertion: "Lorem Basic 4",
+    value: true,
+  },
+  {
+    assertion: "Lorem Basic 5",
+    value: false,
+  },
+  {
+    assertion: "Lorem Basic 6",
+    value: false,
+  },
+  {
+    assertion: "Lorem Basic 7",
+    value: false,
+  },
+  {
+    assertion: "Lorem Basic 8",
+    value: true,
+  },
+  {
+    assertion: "Lorem Basic 9",
+    value: false,
+  },
+  {
+    assertion: "Lorem Basic 10",
+    value: true,
+  },
+];
+
+const medium = [
+  {
+    assertion: "Lorem Medium 1",
+    value: false,
+  },
+  {
+    assertion: "Lorem Medium 2",
+    value: true,
+  },
+  {
+    assertion: "Lorem Medium 3",
+    value: false,
+  },
+  {
+    assertion: "Lorem Medium 4",
+    value: true,
+  },
+  {
+    assertion: "Lorem Medium 5",
+    value: false,
+  },
+  {
+    assertion: "Lorem Medium 6",
+    value: false,
+  },
+  {
+    assertion: "Lorem Medium 7",
+    value: false,
+  },
+  {
+    assertion: "Lorem Medium 8",
+    value: true,
+  },
+  {
+    assertion: "Lorem Medium 9",
+    value: false,
+  },
+  {
+    assertion: "Lorem Medium 10",
+    value: true,
+  },
+];
+
+const advanced = [
+  {
+    assertion: "Lorem Advanced 1",
+    value: false,
+  },
+  {
+    assertion: "Lorem Advanced 2",
+    value: true,
+  },
+  {
+    assertion: "Lorem Advanced 3",
+    value: false,
+  },
+  {
+    assertion: "Lorem Advanced 4",
+    value: true,
+  },
+  {
+    assertion: "Lorem Advanced 5",
+    value: false,
+  },
+  {
+    assertion: "Lorem Advanced 6",
+    value: false,
+  },
+  {
+    assertion: "Lorem Advanced 7",
+    value: false,
+  },
+  {
+    assertion: "Lorem Advanced 8",
+    value: true,
+  },
+  {
+    assertion: "Lorem Advanced 9",
+    value: false,
+  },
+  {
+    assertion: "Lorem Advanced 10",
+    value: true,
+  },
+];
 
 //
 // 2 sections interchangeable through clicks
@@ -73,7 +163,7 @@ const navInfo = document.querySelector(".info-path");
 const anchorTop = document.querySelector(".anchor-top");
 
 //
-// function for allowing custom shortcuts when playing the quiz
+// function for allowing custom keyboard shortcuts when playing the quiz
 const shortcut = (ev) => {
   ev.preventDefault();
 
@@ -89,9 +179,19 @@ const shortcut = (ev) => {
 };
 
 //
-// functions for handling quiz (end it if needed), changing assertions, keeping track
+// functions for handling quiz (end it if needed), changing assertions, keeping track, counting the number of correct answers
+let finalResult = [];
+const checkResult = (arr1, arr2) => {
+  arr1.forEach((el, index) => {
+    if (el === arr2[index]) {
+      finalResult.push("correct answer");
+    }
+  });
+};
+
 const confirmTheEndOfTheQuiz = () => {
-  singleAssertion.textContent = "done";
+  document.removeEventListener("keydown", shortcut);
+  singleAssertion.textContent = `the result is ${finalResult.length}/10`;
   timer.classList.add("invisible");
   tracking.classList.add("invisible");
   submitButton.classList.add("invisible");
@@ -101,19 +201,30 @@ const confirmTheEndOfTheQuiz = () => {
 };
 
 let counter = 1;
+let assertionsValues = [];
+let userValues = [];
 const handleQuiz = (arr) => {
   if (!inputs[0].checked && !inputs[1].checked) {
-    alert("make a choice!");
+    alert("You should make a choice");
   } else {
     inputs.forEach((el) => {
-      el.checked = false;
+      if (el.checked) {
+        if (el.value === "true") {
+          userValues.push(true);
+        } else {
+          userValues.push(false);
+        }
+        el.checked = false;
+      }
     });
 
     if (arr.length > 0) {
       counter++;
       eachTrack.textContent = counter;
-      singleAssertion.textContent = arr.shift();
+      let currentAssertion = arr.shift();
+      singleAssertion.textContent = currentAssertion.assertion;
     } else {
+      checkResult(userValues, assertionsValues);
       confirmTheEndOfTheQuiz();
     }
   }
@@ -123,14 +234,18 @@ const handleQuiz = (arr) => {
 // function for counting down (timer)
 let secondsTimer = document.querySelector(".timer-item-seconds");
 const countDown = (levelSec) => {
-  setInterval(() => {
+  let interval = setInterval(() => {
     levelSec--;
     secondsTimer.textContent = levelSec;
   }, 1000);
 
   let rightTime = levelSec * 1000;
-  setInterval(() => {
+  setTimeout(() => {
+    if (finalResult.length === 0) {
+      checkResult(userValues, assertionsValues);
+    }
     confirmTheEndOfTheQuiz();
+    clearInterval(interval);
   }, rightTime);
 };
 
@@ -140,7 +255,8 @@ const handleTimer = (levelSec) => {
   countDown(levelSec);
 };
 
-// function for starting the right quiz with the right number of seconds
+// THREE behaviours of the THREE buttons:
+// function for starting the right quiz with the right number of seconds;
 const startQuiz = (levelSec) => {
   homePage.classList.add("hidden");
   quizPage.classList.remove("hidden");
@@ -148,38 +264,40 @@ const startQuiz = (levelSec) => {
   handleTimer(levelSec);
 };
 
-//
-// functions/behaviours of the THREE buttons for the THREE levels
+// function for setting the right package of assertions and enabling the submit button
 const setSubmitButton = (level) => {
   submitButton.addEventListener("click", () => {
     handleQuiz(level);
   });
 };
 
-const prepareFirstAssertion = (assert) => {
-  singleAssertion.textContent = assert[0];
-  assert.shift();
+// function allowing the setup and grabbing the boolean values of each assertion
+const prepareFirstAssertion = (arrOfAssertions) => {
+  arrOfAssertions.forEach((assert) => {
+    assertionsValues.push(assert.value);
+  });
+  singleAssertion.textContent = arrOfAssertions[0].assertion;
+  arrOfAssertions.shift();
 };
-//
+
+// THREE buttons for the THREE levels
 // basic-button
 basicQuizButton.addEventListener("click", () => {
   startQuiz(seconds.basic);
-  setSubmitButton(assertions.basic);
-  prepareFirstAssertion(assertions.basic);
+  setSubmitButton(basic);
+  prepareFirstAssertion(basic);
 });
-//
 // medium-button
 mediumQuizButton.addEventListener("click", () => {
   startQuiz(seconds.medium);
-  setSubmitButton(assertions.medium);
-  prepareFirstAssertion(assertions.medium);
+  setSubmitButton(medium);
+  prepareFirstAssertion(medium);
 });
-//
 // advanced-button
 advancedQuizButton.addEventListener("click", () => {
   startQuiz(seconds.advanced);
-  setSubmitButton(assertions.advanced);
-  prepareFirstAssertion(assertions.advanced);
+  setSubmitButton(advanced);
+  prepareFirstAssertion(advanced);
 });
 
 //
