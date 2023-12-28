f1c5433c6e8ce83463e0c7684d18e6be08a99c79
bruno <bruno.lusardi.96@gmail.com>
2023-12-28 18:57:20 +0100

    Refact and Feat: better UX
    Now the user can see the n° of correct answers once the quiz is finished, play it again or change level without reloading the page

--- js/index.js
+++ js/index.js
@@ -135,36 +135,37 @@ const advanced = [
   },
 ];
 
-//
-// 2 sections interchangeable through clicks
+// 2 sections interchangeable
 // 1 homepage
 const homePage = document.querySelector(".total-homepage");
+const headings = document.querySelector(".heading");
+const containerResult = document.querySelector(".result");
+const resultUser = document.querySelector(".result-user");
 const basicQuizButton = document.querySelector(".level-basic");
 const mediumQuizButton = document.querySelector(".level-medium");
 const advancedQuizButton = document.querySelector(".level-advanced");
 // 2 quiz
 const quizPage = document.querySelector(".total-quiz");
 const timer = document.querySelector(".timer");
-const tracking = document.querySelector(".assertions-tracking");
+const secondsTimer = document.querySelector(".timer-item-seconds");
 const eachTrack = document.querySelector(".each-assertion");
 const singleAssertion = document.querySelector(".assertion");
-const labels = document.querySelectorAll("label");
 const inputs = document.querySelectorAll("input");
 const submitButton = document.querySelector(".button-submit-js");
 //
 // section-info (block)
 const sectionInfo = document.querySelector(".info");
 //
-// header items (two items)
+// header items
 const logo = document.querySelector(".navbar-logo");
 const navInfo = document.querySelector(".info-path");
 //
-// footer item (one item)
+// footer item
 const anchorTop = document.querySelector(".anchor-top");
 
 //
-// function for allowing custom keyboard shortcuts when playing the quiz
-const shortcut = (ev) => {
+// function for allowing custom keyboard shortcuts
+const shortcuts = (ev) => {
   ev.preventDefault();
 
   if (ev.key === "t" && ev.ctrlKey && ev.altKey) {
@@ -178,9 +179,40 @@ const shortcut = (ev) => {
   }
 };
 
-//
-// functions for handling quiz (end it if needed), changing assertions, keeping track, counting the number of correct answers
+// show quiz
+const showQuiz = () => {
+  document.addEventListener("keydown", shortcuts);
+  homePage.classList.add("hidden");
+  quizPage.classList.remove("hidden");
+};
+
+// hide quiz
+const hideQuiz = () => {
+  document.removeEventListener("keydown", shortcuts);
+  quizPage.classList.add("hidden");
+  homePage.classList.remove("hidden");
+};
+
+// end quiz
+const confirmTheEndOfTheQuiz = () => {
+  index = 1;
+  eachTrack.textContent = 1;
+  resultUser.textContent = `The result is ${finalResult.length}/10`;
+  headings.classList.add("hidden");
+  containerResult.classList.remove("hidden");
+};
+
+// arrays to grab data to check user's answers
 let finalResult = [];
+let assertionsValues = [];
+let userValues = [];
+const restoreArrays = () => {
+  assertionsValues = [];
+  userValues = [];
+  finalResult = [];
+};
+
+// func to calculate user's correct answers
 const checkResult = (arr1, arr2) => {
   arr1.forEach((el, index) => {
     if (el === arr2[index]) {
@@ -189,124 +221,102 @@ const checkResult = (arr1, arr2) => {
   });
 };
 
-const confirmTheEndOfTheQuiz = () => {
-  document.removeEventListener("keydown", shortcut);
-  singleAssertion.textContent = `the result is ${finalResult.length}/10`;
-  timer.classList.add("invisible");
-  tracking.classList.add("invisible");
-  submitButton.classList.add("invisible");
-  labels.forEach((el) => {
-    el.classList.add("invisible");
-  });
-};
-
-let counter = 1;
-let assertionsValues = [];
-let userValues = [];
-const handleQuiz = (arr) => {
-  if (!inputs[0].checked && !inputs[1].checked) {
-    alert("You should make a choice");
-  } else {
-    inputs.forEach((el) => {
-      if (el.checked) {
-        if (el.value === "true") {
-          userValues.push(true);
-        } else {
-          userValues.push(false);
-        }
-        el.checked = false;
-      }
-    });
-
-    if (arr.length > 0) {
-      counter++;
-      eachTrack.textContent = counter;
-      let currentAssertion = arr.shift();
-      singleAssertion.textContent = currentAssertion.assertion;
-    } else {
-      checkResult(userValues, assertionsValues);
-      confirmTheEndOfTheQuiz();
-    }
-  }
-};
-
-//
 // function for counting down (timer)
-let secondsTimer = document.querySelector(".timer-item-seconds");
-const countDown = (levelSec) => {
-  let interval = setInterval(() => {
+let interval;
+let timeout;
+const handleTimerCountDown = (levelSec) => {
+  interval = setInterval(() => {
     levelSec--;
     secondsTimer.textContent = levelSec;
   }, 1000);
 
   let rightTime = levelSec * 1000;
-  setTimeout(() => {
+  timeout = setTimeout(() => {
     if (finalResult.length === 0) {
       checkResult(userValues, assertionsValues);
     }
     confirmTheEndOfTheQuiz();
+    restoreArrays();
+    hideQuiz();
     clearInterval(interval);
   }, rightTime);
 };
 
-// function for handling the amount of seconds (timer)
-const handleTimer = (levelSec) => {
-  secondsTimer.textContent = levelSec;
-  countDown(levelSec);
-};
+// functions for handling the entire quiz (end it if needed), changing assertions and keeping track of them, storing user's answers
+let index = 1;
+const handleQuiz = (arr) => {
+  if (!inputs[0].checked && !inputs[1].checked) {
+    return;
+  } else {
+    inputs.forEach((input) => {
+      if (input.checked) {
+        if (input.value === "true") {
+          userValues.push(true);
+        } else {
+          userValues.push(false);
+        }
+        input.checked = false;
+      }
+    });
 
-// THREE behaviours of the THREE buttons:
-// function for starting the right quiz with the right number of seconds;
-const startQuiz = (levelSec) => {
-  homePage.classList.add("hidden");
-  quizPage.classList.remove("hidden");
-  document.addEventListener("keydown", shortcut);
-  handleTimer(levelSec);
+    if (userValues.length === arr.length) {
+      checkResult(userValues, assertionsValues);
+      confirmTheEndOfTheQuiz();
+      restoreArrays();
+      hideQuiz();
+      clearInterval(interval);
+      clearTimeout(timeout);
+    } else {
+      let currentItem = arr[index];
+      singleAssertion.textContent = currentItem?.assertion;
+      index++;
+      eachTrack.textContent = index;
+    }
+  }
 };
-
-// function for setting the right package of assertions and enabling the submit button
-const setSubmitButton = (level) => {
-  submitButton.addEventListener("click", () => {
-    handleQuiz(level);
-  });
-};
+submitButton.addEventListener("click", () => {
+  handleQuiz(array);
+});
 
 // function allowing the setup and grabbing the boolean values of each assertion
-const prepareFirstAssertion = (arrOfAssertions) => {
+let array;
+const prepareArrayForTheQuiz = (arrOfAssertions) => {
   arrOfAssertions.forEach((assert) => {
-    assertionsValues.push(assert.value);
+    assertionsValues.push(assert?.value);
   });
-  singleAssertion.textContent = arrOfAssertions[0].assertion;
-  arrOfAssertions.shift();
+  array = [...arrOfAssertions];
+  singleAssertion.textContent = array[0]?.assertion;
+};
+
+// function for starting the right quiz with the right number of seconds;
+const startQuiz = (levelSec) => {
+  showQuiz();
+  secondsTimer.textContent = levelSec;
+  handleTimerCountDown(levelSec);
 };
 
 // THREE buttons for the THREE levels
 // basic-button
 basicQuizButton.addEventListener("click", () => {
   startQuiz(seconds.basic);
-  setSubmitButton(basic);
-  prepareFirstAssertion(basic);
+  prepareArrayForTheQuiz(basic);
 });
 // medium-button
 mediumQuizButton.addEventListener("click", () => {
   startQuiz(seconds.medium);
-  setSubmitButton(medium);
-  prepareFirstAssertion(medium);
+  prepareArrayForTheQuiz(medium);
 });
 // advanced-button
 advancedQuizButton.addEventListener("click", () => {
   startQuiz(seconds.advanced);
-  setSubmitButton(advanced);
-  prepareFirstAssertion(advanced);
+  prepareArrayForTheQuiz(advanced);
 });
 
 //
 //function/behaviour of the logo
 const doNotAllowQuiz = () => {
+  hideQuiz();
   window.location.reload();
-  homePage.classList.remove("hidden");
-  quizPage.classList.add("hidden");
-  document.removeEventListener("keydown", shortcut);
 };
 logo.addEventListener("click", doNotAllowQuiz);
 
