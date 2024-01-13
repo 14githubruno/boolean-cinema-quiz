6af5c2289a364a70555951dfe537591601579b9d
bruno <bruno.lusardi.96@gmail.com>
2024-01-13 16:59:12 +0100

    New js module - Refact data structure
    Data (seconds and assertions) in a separate file. 3 arrays of assertions grouped into an object

---
+++ js/data.js
@@ -0,0 +1,138 @@
+// obj of seconds
+const secondsObject = {
+  basic: 30,
+  medium: 25,
+  advanced: 20,
+};
+
+// obj of assertions
+const assertionsObject = {
+  basic: [
+    {
+      assertion: "Lorem Basic 1",
+      value: true,
+    },
+    {
+      assertion: "Lorem Basic 2",
+      value: true,
+    },
+    {
+      assertion: "Lorem Basic 3",
+      value: false,
+    },
+    {
+      assertion: "Lorem Basic 4",
+      value: true,
+    },
+    {
+      assertion: "Lorem Basic 5",
+      value: false,
+    },
+    {
+      assertion: "Lorem Basic 6",
+      value: false,
+    },
+    {
+      assertion: "Lorem Basic 7",
+      value: false,
+    },
+    {
+      assertion: "Lorem Basic 8",
+      value: true,
+    },
+    {
+      assertion: "Lorem Basic 9",
+      value: false,
+    },
+    {
+      assertion: "Lorem Basic 10",
+      value: true,
+    },
+  ],
+  medium: [
+    {
+      assertion: "Lorem Medium 1",
+      value: false,
+    },
+    {
+      assertion: "Lorem Medium 2",
+      value: true,
+    },
+    {
+      assertion: "Lorem Medium 3",
+      value: false,
+    },
+    {
+      assertion: "Lorem Medium 4",
+      value: true,
+    },
+    {
+      assertion: "Lorem Medium 5",
+      value: false,
+    },
+    {
+      assertion: "Lorem Medium 6",
+      value: false,
+    },
+    {
+      assertion: "Lorem Medium 7",
+      value: false,
+    },
+    {
+      assertion: "Lorem Medium 8",
+      value: true,
+    },
+    {
+      assertion: "Lorem Medium 9",
+      value: false,
+    },
+    {
+      assertion: "Lorem Medium 10",
+      value: true,
+    },
+  ],
+  advanced: [
+    {
+      assertion: "Lorem Advanced 1",
+      value: false,
+    },
+    {
+      assertion: "Lorem Advanced 2",
+      value: true,
+    },
+    {
+      assertion: "Lorem Advanced 3",
+      value: false,
+    },
+    {
+      assertion: "Lorem Advanced 4",
+      value: true,
+    },
+    {
+      assertion: "Lorem Advanced 5",
+      value: false,
+    },
+    {
+      assertion: "Lorem Advanced 6",
+      value: false,
+    },
+    {
+      assertion: "Lorem Advanced 7",
+      value: false,
+    },
+    {
+      assertion: "Lorem Advanced 8",
+      value: true,
+    },
+    {
+      assertion: "Lorem Advanced 9",
+      value: false,
+    },
+    {
+      assertion: "Lorem Advanced 10",
+      value: true,
+    },
+  ],
+};
+
+export { secondsObject, assertionsObject };
